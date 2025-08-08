626841f1b97013046168c09659f3e6b8f79aaad9
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-08 15:17:15 +0530

    Auto commit at 2025-08-08 15:17:14

--- Bank  Management System/Account1.cs
+++ Bank  Management System/Account1.cs
@@ -1,6 +1,12 @@
-﻿namespace Bank__Management_System
+﻿using System;
+
+namespace Bank__Management_System
 {
     internal class Account
     {
+        internal void Show()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
