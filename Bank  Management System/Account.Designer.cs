91492f6c46a812412878496e412a9d1a0ff06440
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-08 15:25:50 +0530

    Auto commit at 2025-08-08 15:25:49

--- Bank  Management System/Account.Designer.cs
+++ Bank  Management System/Account.Designer.cs
@@ -278,7 +278,6 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1280, 785);
             this.Controls.Add(this.dateTimePicker1);
@@ -299,7 +298,6 @@
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.btnSave);
             this.DoubleBuffered = true;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "txtDateOpened";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Account";
