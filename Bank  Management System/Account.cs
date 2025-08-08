09f8590c4cd73d715d1aeafa18d95f1038682abe
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-08 16:56:06 +0530

    Auto commit at 2025-08-08 16:56:06

--- Bank  Management System/Account.cs
+++ Bank  Management System/Account.cs
@@ -11,6 +11,10 @@ namespace Bank__Management_System
         {
             InitializeComponent();
 
+            // Wire up btnAdd click event
+            btnAdd.Click += btnAdd_Click;
+            btnSave.Click += btnSave_Click;
+            btnUpdate.Click += btnUpdate_Click;
         }
 
         private void Account_Load(object sender, EventArgs e)
