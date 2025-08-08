391b88c773dba0a0436a72b5af0977f647eda4e8
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-08 14:52:52 +0530

    Auto commit at 2025-08-08 14:52:52

--- Bank  Management System/Account.cs
+++ Bank  Management System/Account.cs
@@ -73,6 +73,27 @@ namespace Bank__Management_System
 
             MessageBox.Show("Record added Successfully");
         }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(@"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False"))
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand(
+                    "UPDATE Customer SET Customer_Name = @Customer_Name, Phone = @Phone, Email = @Email, Address = @Address, Username = @Username, Password = @Password WHERE Customer_ID = @Customer_ID", con);
+
+                cmd.Parameters.AddWithValue("@Account_ID", int.Parse(txtAccountID.Text));
+                cmd.Parameters.AddWithValue("@Account_Type", txtAccountType.Text);
+                cmd.Parameters.AddWithValue("@Balance", txtBalance.Text);
+                cmd.Parameters.AddWithValue("@Date_Opened", dateTimePicker1.Value);
+                cmd.Parameters.AddWithValue("@Customer_name", txtCustomerName.Text);
+
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("Record Updated Successfully");
+        }
     }
 }
  
