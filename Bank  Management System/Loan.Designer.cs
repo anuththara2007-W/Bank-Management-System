73626efc84ec6d761c80c60919838c3ad635f166
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-10 15:09:13 +0530

    Auto commit at 2025-08-10 15:09:13

--- Bank  Management System/Loan.Designer.cs
+++ Bank  Management System/Loan.Designer.cs
@@ -190,7 +190,6 @@
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(337, 27);
             this.dateTimePicker1.TabIndex = 29;
-            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // dataGridView1
             // 
