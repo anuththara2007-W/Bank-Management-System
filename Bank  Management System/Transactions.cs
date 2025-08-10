24a92ef97fe7d94190e5f111b8a6f6ae2a434673
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-10 13:40:38 +0530

    Auto commit at 2025-08-10 13:40:38

--- Bank  Management System/Transactions.cs
+++ Bank  Management System/Transactions.cs
@@ -1,30 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Linq;
 
 namespace Bank__Management_System
 {
     public partial class Transactions : Form
     {
+        string connString = @"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False";
+
         public Transactions()
         {
             InitializeComponent();
         }
 
-
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
             dateTimePicker1.CustomFormat = "dd/MM/yyyy";
-
         }
+
         private void dateTimePicker1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Back)
@@ -35,14 +29,14 @@ namespace Bank__Management_System
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-
             try
             {
-                using (SqlConnection con = new SqlConnection(@"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False"))
+                using (SqlConnection con = new SqlConnection(connString))
                 {
                     con.Open();
-
-                    SqlCommand cmd = new SqlCommand("INSERT INTO Transactions  VALUES (@tid, @Transaction_Type, @Amount, @Transaction_Date, @Account_ID)", con);
+                    SqlCommand cmd = new SqlCommand(
+                        "INSERT INTO Transactions (TID, Transaction_Type, Amount, Transaction_Date, Account_ID) " +
+                        "VALUES (@TID, @Transaction_Type, @Amount, @Transaction_Date, @Account_ID)", con);
 
                     cmd.Parameters.AddWithValue("@TID", int.Parse(txtTransactionID.Text));
                     cmd.Parameters.AddWithValue("@Transaction_Type", txtTransactionType.Text);
@@ -51,13 +45,9 @@ namespace Bank__Management_System
                     cmd.Parameters.AddWithValue("@Account_ID", int.Parse(txtAccountID.Text));
 
                     cmd.ExecuteNonQuery();
-                    con.Close();
-
                     MessageBox.Show("Record saved successfully");
 
-                    // Clear inputs after saving
-                    btnSave_Click(null, null);
-
+                    ClearFields();
                     LoadTransactions();
                 }
             }
@@ -69,12 +59,10 @@ namespace Bank__Management_System
 
         private void LoadTransactions()
         {
-            string connString = @"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False";
             using (SqlConnection con = new SqlConnection(connString))
             {
                 con.Open();
-                SqlCommand cmd = new SqlCommand("SELECT * FROM Transactions", con);
-                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Transactions", con);
                 DataTable dt = new DataTable();
                 da.Fill(dt);
                 dataGridView1.DataSource = dt;
@@ -83,28 +71,30 @@ namespace Bank__Management_System
 
         private void btnClear_Click(object sender, EventArgs e)
         {
+            ClearFields();
+        }
+
+        private void ClearFields()
+        {
             txtTransactionID.Clear();
             txtTransactionType.Clear();
             txtAmount.Clear();
             txtAccountID.Clear();
             dateTimePicker1.Value = DateTime.Today;
             dateTimePicker1.CustomFormat = "dd/MM/yyyy";
-
-
-            txtAccountID.Focus();
+            txtTransactionID.Focus();
         }
 
         private void btnUpdate_Click(object sender, EventArgs e)
         {
             try
             {
-                using (SqlConnection con = new SqlConnection(@"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False"))
+                using (SqlConnection con = new SqlConnection(connString))
                 {
                     con.Open();
-
                     SqlCommand cmd = new SqlCommand(
-                        "UPDATE transactions SET transaction_type = @transaction_type, amount = @amount, transaction_date = @transaction_date, customer_name = @customer_name WHERE tid = @tid",
-                        con);
+                        "UPDATE Transactions SET Transaction_Type = @Transaction_Type, Amount = @Amount, " +
+                        "Transaction_Date = @Transaction_Date, Account_ID = @Account_ID WHERE TID = @TID", con);
 
                     cmd.Parameters.AddWithValue("@TID", int.Parse(txtTransactionID.Text));
                     cmd.Parameters.AddWithValue("@Transaction_Type", txtTransactionType.Text);
@@ -113,7 +103,6 @@ namespace Bank__Management_System
                     cmd.Parameters.AddWithValue("@Account_ID", int.Parse(txtAccountID.Text));
 
                     int rows = cmd.ExecuteNonQuery();
-                    con.Close();
 
                     if (rows > 0)
                     {
@@ -122,7 +111,7 @@ namespace Bank__Management_System
                     }
                     else
                     {
-                        MessageBox.Show("No record found with the specified Account ID.");
+                        MessageBox.Show("No record found with the specified Transaction ID.");
                     }
                 }
             }
@@ -136,14 +125,13 @@ namespace Bank__Management_System
         {
             try
             {
-                using (SqlConnection con = new SqlConnection(@"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False"))
+                using (SqlConnection con = new SqlConnection(connString))
                 {
                     con.Open();
-                    SqlCommand cmd = new SqlCommand("DELETE FROM transactions WHERE tid = @tid", con);
-                    cmd.Parameters.AddWithValue("@tid", int.Parse(txtTransactionID.Text));
+                    SqlCommand cmd = new SqlCommand("DELETE FROM Transactions WHERE TID = @TID", con);
+                    cmd.Parameters.AddWithValue("@TID", int.Parse(txtTransactionID.Text));
 
                     int rows = cmd.ExecuteNonQuery();
-                    con.Close();
 
                     if (rows > 0)
                     {
@@ -152,7 +140,7 @@ namespace Bank__Management_System
                     }
                     else
                     {
-                        MessageBox.Show("No record found with the specified Account ID.");
+                        MessageBox.Show("No record found with the specified Transaction ID.");
                     }
                 }
             }
@@ -162,5 +150,4 @@ namespace Bank__Management_System
             }
         }
     }
-    }
-
+}
