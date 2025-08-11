2e209259e69638e4a6ecc1f2ae9b2e6ab2d408fa
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-11 20:22:19 +0530

    Project updated at 2025-08-11 20:22:19

--- Bank  Management System/Dashboard.cs
+++ Bank  Management System/Dashboard.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Bank__Management_System
@@ -25,45 +18,63 @@ namespace Bank__Management_System
             display3();
         }
 
-
         private void display1()
         {
-            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False");
-            con.Open();
-            SqlCommand comm = new SqlCommand("SELECT COUNT(*) FROM emptab", con);
-            int count = (int)comm.ExecuteScalar();
-            if (count > 0)
-                lblCount1.Text = count.ToString();
-            else
-                lblCount1.Text = "0";
-            con.Close();
+            using (SqlConnection con = new SqlConnection(@"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False"))
+            {
+                con.Open();
+                SqlCommand comm = new SqlCommand("SELECT COUNT(*) FROM emptab", con);
+                Int32 count = Convert.ToInt32(comm.ExecuteScalar());
+                if (count > 0)
+                {
+                    lblCount1.Text = count.ToString();
+                }
+                else
+                {
+                    lblCount1.Text = "0";
+                }
+                con.Close();
+            }
         }
 
         private void display2()
         {
-            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False");
-            con.Open();
-            SqlCommand comm = new SqlCommand("SELECT COUNT(*) FROM loantab", con);
-            int count = (int)comm.ExecuteScalar();
-            if (count > 0)
-                lblCount2.Text = count.ToString();
-            else
-                lblCount2.Text = "0";
-            con.Close();
+            using (SqlConnection con = new SqlConnection(@"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False"))
+            {
+                con.Open();
+                SqlCommand comm = new SqlCommand("SELECT COUNT(*) FROM loantab", con);
+                Int32 count = Convert.ToInt32(comm.ExecuteScalar());
+                if (count > 0)
+                {
+                    lblCount2.Text = count.ToString();
+                }
+                else
+                {
+                    lblCount2.Text = "0";
+                }
+                con.Close();
+            }
         }
 
         private void display3()
         {
-            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False");
-            con.Open();
-            SqlCommand comm = new SqlCommand("SELECT COUNT(*) FROM customertab", con);
-            int count = (int)comm.ExecuteScalar();
-            if (count > 0)
-                lblCount3.Text = count.ToString();
-            else
-                lblCount3.Text = "0";
-            con.Close();
+            using (SqlConnection con = new SqlConnection(@"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False"))
+            {
+                con.Open();
+                SqlCommand comm = new SqlCommand("SELECT COUNT(*) FROM customertab", con);
+                Int32 count = Convert.ToInt32(comm.ExecuteScalar());
+                if (count > 0)
+                {
+                    lblCount3.Text = count.ToString();
+                }
+                else
+                {
+                    lblCount3.Text = "0";
+                }
+                con.Close();
+            }
         }
+
         private void btnGoBack_Click(object sender, EventArgs e)
         {
             Login gonow = new Login();
