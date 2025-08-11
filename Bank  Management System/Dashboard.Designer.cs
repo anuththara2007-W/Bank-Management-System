45d3f78abc1b14e92907c845a0ee4fafbe271ea3
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-11 19:43:19 +0530

    Project updated at 2025-08-11 19:43:19

--- Bank  Management System/Dashboard.Designer.cs
+++ Bank  Management System/Dashboard.Designer.cs
@@ -31,7 +31,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.btnGoBack = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -48,22 +47,13 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(255)))), ((int)(((byte)(237)))));
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(403, 171);
+            this.label2.Location = new System.Drawing.Point(410, 171);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(64, 69);
             this.label2.TabIndex = 1;
             this.label2.Text = "0";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(684, 171);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 69);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "0";
             // 
             // label4
             // 
@@ -100,7 +90,6 @@
             this.ClientSize = new System.Drawing.Size(1280, 785);
             this.Controls.Add(this.btnGoBack);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Dashboard";
@@ -114,7 +103,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnGoBack;
     }
