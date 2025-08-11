77617518b4e223620afc76dd099afa19500b4ce4
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-11 19:22:14 +0530

    Project updated at 2025-08-11 19:22:14

--- Bank  Management System/Employee.cs
+++ Bank  Management System/Employee.cs
@@ -33,7 +33,7 @@ namespace Bank__Management_System
                 DataTable dt = new DataTable();
                 da.Fill(dt);
                 dataGridView1.DataSource = dt;
-
+                LoadEmployee();
             }
         }
 
