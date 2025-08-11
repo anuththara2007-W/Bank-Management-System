3728cea5e5c11cd5d1c8b5a3d3e447d633b62caf
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-11 19:43:27 +0530

    Project updated at 2025-08-11 19:43:27

--- Bank  Management System/Dashboard.Designer.cs
+++ Bank  Management System/Dashboard.Designer.cs
@@ -31,7 +31,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.btnGoBack = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -54,16 +53,6 @@
             this.label2.Size = new System.Drawing.Size(64, 69);
             this.label2.TabIndex = 1;
             this.label2.Text = "0";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(965, 171);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(64, 69);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "0";
             // 
             // btnGoBack
             // 
@@ -89,7 +78,6 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1280, 785);
             this.Controls.Add(this.btnGoBack);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Dashboard";
@@ -103,7 +91,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnGoBack;
     }
 }
