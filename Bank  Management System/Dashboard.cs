0faee6859f64d451c5c9e5cbf205fec2dd4a9ee7
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-11 19:46:12 +0530

    Project updated at 2025-08-11 19:46:12

--- Bank  Management System/Dashboard.cs
+++ Bank  Management System/Dashboard.cs
@@ -25,7 +25,7 @@ namespace Bank__Management_System
         private void btnGoBack_Click(object sender, EventArgs e)
         {
             Form1 logOut = new Form1();
-            admins.Show();
+            Form1.Show();
             this.Hide();
         }
     }
