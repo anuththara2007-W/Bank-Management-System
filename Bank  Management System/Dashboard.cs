8ee1c1261909303bf698559124b8ef0a1094c1b8
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-11 19:55:26 +0530

    Project updated at 2025-08-11 19:55:26

--- Bank  Management System/Dashboard.cs
+++ Bank  Management System/Dashboard.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,8 +20,11 @@ namespace Bank__Management_System
 
         private void Dashboard_Load(object sender, EventArgs e)
         {
-
-        }
+            using (SqlConnection con = new SqlConnection(@"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False"))
+            {
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Employee", con);
+            }
 
         private void btnGoBack_Click(object sender, EventArgs e)
         {
