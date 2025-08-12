ccdb9d2a910efda221fea11724c3e896528652c5
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-12 20:42:14 +0530

    Project updated at 2025-08-12 20:42:14

--- Bank  Management System/AccountPicker .Designer.cs
+++ Bank  Management System/AccountPicker .Designer.cs
@@ -52,7 +52,6 @@
             this.dgvAccounts.RowTemplate.Height = 24;
             this.dgvAccounts.Size = new System.Drawing.Size(1233, 671);
             this.dgvAccounts.TabIndex = 3;
-            this.dgvAccounts.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvAccounts_Click);
             this.dgvAccounts.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvAccounts_CellDoubleClick);
             // 
             // AccountPicker
