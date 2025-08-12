d6636391b59dab761849fccdaa0a8c6587446dd2
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-12 20:28:50 +0530

    Project updated at 2025-08-12 20:28:50

--- Bank  Management System/CustomerPicker.Designer.cs
+++ Bank  Management System/CustomerPicker.Designer.cs
@@ -28,19 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvCustomers = new System.Windows.Forms.DataGridView();
+            this.btnSelect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvCustomers
+            // 
+            this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomers.Location = new System.Drawing.Point(22, 22);
+            this.dgvCustomers.Name = "dgvCustomers";
+            this.dgvCustomers.RowHeadersWidth = 51;
+            this.dgvCustomers.RowTemplate.Height = 24;
+            this.dgvCustomers.Size = new System.Drawing.Size(1233, 671);
+            this.dgvCustomers.TabIndex = 0;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(506, 720);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(236, 44);
+            this.btnSelect.TabIndex = 2;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
             // 
             // CustomerPicker
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1280, 785);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.dgvCustomers);
             this.Name = "CustomerPicker";
             this.Text = "CustomerPicker";
+            this.Load += new System.EventHandler(this.CustomerPicker_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvCustomers;
+        public System.Windows.Forms.Button btnSelect;
     }
 }
--- Bank  Management System/CustomerPicker.cs
+++ Bank  Management System/CustomerPicker.cs
@@ -1,20 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
-namespace Bank__Management_System
+namespace BankApp
 {
     public partial class CustomerPicker : Form
     {
+        public int SelectedCustomerID { get; private set; }
+        public string ConnectionString = @"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False";
+
         public CustomerPicker()
         {
             InitializeComponent();
+        }
+
+        private void CustomerPicker_Load(object sender, EventArgs e)
+        {
+            LoadCustomers();
+        }
+
+        private void LoadCustomers()
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                string query = "SELECT Customer_ID, Customer_Name, Phone, Email FROM Customers";
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvCustomers.DataSource = dt;
+            }
+        }
+
+        private void dgvCustomers_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                SelectedCustomerID = Convert.ToInt32(dgvCustomers.Rows[e.RowIndex].Cells["Customer_ID"].Value);
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            if (dgvCustomers.CurrentRow != null)
+            {
+                SelectedCustomerID = Convert.ToInt32(dgvCustomers.CurrentRow.Cells["Customer_ID"].Value);
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
         }
     }
 }
