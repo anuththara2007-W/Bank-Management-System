57146e7c1f57da2ef47a89034db827a9af97dad3
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-13 15:32:35 +0530

    Project updated at 2025-08-13 15:32:35

--- Bank  Management System/CustomerDashboard.Designer.cs
+++ Bank  Management System/CustomerDashboard.Designer.cs
@@ -150,6 +150,7 @@
             this.btnProfile.TabIndex = 10;
             this.btnProfile.Text = "Profile";
             this.btnProfile.UseVisualStyleBackColor = true;
+            this.btnProfile.Click += new System.EventHandler(this.btnProfile_Click);
             // 
             // btnChangePassword
             // 
