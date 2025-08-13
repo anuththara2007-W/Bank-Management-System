7c727801cf43622077522d36e0d42aea6aa3ef41
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-13 10:49:28 +0530

    Project updated at 2025-08-13 10:49:28

--- Bank  Management System/Landing.Designer.cs
+++ Bank  Management System/Landing.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Landing));
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -44,12 +45,21 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(753, 356);
+            this.button1.Location = new System.Drawing.Point(275, 618);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(201, 116);
+            this.button1.Size = new System.Drawing.Size(201, 32);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1095, 618);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(201, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Landing
             // 
@@ -58,6 +68,7 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1280, 785);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -72,5 +83,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
