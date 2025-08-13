8025224dfd01f5c5f241930d6ada47c4790fa07a
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-13 14:19:57 +0530

    Project updated at 2025-08-13 14:19:57

--- Bank  Management System/TransferFunds.Designer.cs
+++ Bank  Management System/TransferFunds.Designer.cs
@@ -30,17 +30,81 @@ namespace BankApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnWithdraw = new System.Windows.Forms.Button();
+            this.btnDeposit = new System.Windows.Forms.Button();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.cmbAccount = new System.Windows.Forms.ComboBox();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.lblFromAccount = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnWithdraw
+            // 
+            this.btnWithdraw.Location = new System.Drawing.Point(460, 299);
+            this.btnWithdraw.Name = "btnWithdraw";
+            this.btnWithdraw.Size = new System.Drawing.Size(189, 30);
+            this.btnWithdraw.TabIndex = 11;
+            this.btnWithdraw.Text = "Withdraw";
+            this.btnWithdraw.UseVisualStyleBackColor = true;
+            // 
+            // btnDeposit
+            // 
+            this.btnDeposit.Location = new System.Drawing.Point(152, 299);
+            this.btnDeposit.Name = "btnDeposit";
+            this.btnDeposit.Size = new System.Drawing.Size(189, 30);
+            this.btnDeposit.TabIndex = 10;
+            this.btnDeposit.Text = "Deposit ";
+            this.btnDeposit.UseVisualStyleBackColor = true;
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(289, 195);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(360, 22);
+            this.txtAmount.TabIndex = 9;
+            // 
+            // cmbAccount
+            // 
+            this.cmbAccount.FormattingEnabled = true;
+            this.cmbAccount.Location = new System.Drawing.Point(299, 121);
+            this.cmbAccount.Name = "cmbAccount";
+            this.cmbAccount.Size = new System.Drawing.Size(337, 24);
+            this.cmbAccount.TabIndex = 8;
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Location = new System.Drawing.Point(166, 198);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(52, 16);
+            this.lblAmount.TabIndex = 7;
+            this.lblAmount.Text = "Amount";
+            // 
+            // lblFromAccount
+            // 
+            this.lblFromAccount.AutoSize = true;
+            this.lblFromAccount.Location = new System.Drawing.Point(154, 121);
+            this.lblFromAccount.Name = "lblFromAccount";
+            this.lblFromAccount.Size = new System.Drawing.Size(96, 16);
+            this.lblFromAccount.TabIndex = 6;
+            this.lblFromAccount.Text = "Select Account";
             // 
             // TransferFunds
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnWithdraw);
+            this.Controls.Add(this.btnDeposit);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.cmbAccount);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.lblFromAccount);
             this.Name = "TransferFunds";
             this.Text = "TransferFunds";
             this.Load += new System.EventHandler(this.TransferFunds_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -50,5 +114,12 @@ namespace BankApp
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnWithdraw;
+        private System.Windows.Forms.Button btnDeposit;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.ComboBox cmbAccount;
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.Label lblFromAccount;
     }
 }
