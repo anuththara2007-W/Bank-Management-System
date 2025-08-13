6d35591970c7118e4769c5c9e26dd14f9bcfa793
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-13 14:43:44 +0530

    Project updated at 2025-08-13 14:43:44

--- Bank  Management System/CustomerMain.Designer.cs
+++ Bank  Management System/CustomerMain.Designer.cs
@@ -1,23 +1,78 @@
-﻿using System;
-using System.Windows.Forms;
-
-namespace BankApp
+﻿namespace Bank__Management_System
 {
-    public partial class CustomerMain : Form
+    partial class CustomerMain
     {
-        public CustomerMain()
+        private System.ComponentModel.IContainer components = null;
+        private Label lblCustomerName;
+        private Button btnDashboard;
+        private Button btnAccounts;
+        private Button btnTransactions;
+        private Button btnDepositWithdraw;
+        private Button btnTransfer;
+        private Button btnLoans;
+        private Button btnLoanRequest;
+        private Button btnProfile;
+        private Button btnChangePassword;
+        private Button btnSupport;
+
+        protected override void Dispose(bool disposing)
         {
-            InitializeComponent();
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
         }
 
         private void InitializeComponent()
         {
-            throw new NotImplementedException();
-        }
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.btnAccounts = new System.Windows.Forms.Button();
+            this.btnTransactions = new System.Windows.Forms.Button();
+            this.btnDepositWithdraw = new System.Windows.Forms.Button();
+            this.btnTransfer = new System.Windows.Forms.Button();
+            this.btnLoans = new System.Windows.Forms.Button();
+            this.btnLoanRequest = new System.Windows.Forms.Button();
+            this.btnProfile = new System.Windows.Forms.Button();
+            this.btnChangePassword = new System.Windows.Forms.Button();
+            this.btnSupport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
 
-        private void CustomerMain_Load(object sender, EventArgs e)
-        {
-            // Code to run when form loads
+            // lblCustomerName
+            this.lblCustomerName.AutoSize = true;
+            this.lblCustomerName.Location = new System.Drawing.Point(20, 20);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(120, 20);
+            this.lblCustomerName.Text = "Welcome, User";
+
+            // Buttons setup (just example for one, repeat for all)
+            this.btnDashboard.Text = "Dashboard";
+            this.btnDashboard.Location = new System.Drawing.Point(20, 60);
+            this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
+
+            this.btnAccounts.Text = "My Accounts";
+            this.btnAccounts.Location = new System.Drawing.Point(20, 100);
+            this.btnAccounts.Click += new System.EventHandler(this.btnAccounts_Click);
+
+            // Add more buttons here in similar way...
+
+            // CustomerMain Form
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.lblCustomerName);
+            this.Controls.Add(this.btnDashboard);
+            this.Controls.Add(this.btnAccounts);
+            this.Controls.Add(this.btnTransactions);
+            this.Controls.Add(this.btnDepositWithdraw);
+            this.Controls.Add(this.btnTransfer);
+            this.Controls.Add(this.btnLoans);
+            this.Controls.Add(this.btnLoanRequest);
+            this.Controls.Add(this.btnProfile);
+            this.Controls.Add(this.btnChangePassword);
+            this.Controls.Add(this.btnSupport);
+            this.Name = "CustomerMain";
+            this.Text = "Customer Main Menu";
+            this.Load += new System.EventHandler(this.CustomerMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
     }
 }
