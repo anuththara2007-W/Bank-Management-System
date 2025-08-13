63da42a31e03e73cce06a4b02b9770990858ad21
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-13 13:33:40 +0530

    Project updated at 2025-08-13 13:33:40

---
+++ Bank  Management System/DatabaseHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace BankApp
+{
+    public static class DatabaseHelper
+    {
+        public static string ConnectionString =
+            @"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+    }
+}
