f1a20855841dcfd451780472f44fb5e99f56a809
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-13 14:44:06 +0530

    Project updated at 2025-08-13 14:44:06

--- Bank  Management System/MyAccounts1.cs
+++ Bank  Management System/MyAccounts1.cs
@@ -1,9 +1,16 @@
-﻿namespace Bank__Management_System
+﻿using System;
+
+namespace Bank__Management_System
 {
     internal class MyAccounts
     {
         public MyAccounts()
         {
+        }
+
+        internal void Show()
+        {
+            throw new NotImplementedException();
         }
     }
 }
