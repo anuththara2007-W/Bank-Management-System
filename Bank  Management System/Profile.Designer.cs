65d5233e0c0ff4251e4e506fe7138452f53f97c4
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-13 14:28:35 +0530

    Project updated at 2025-08-13 14:28:35

--- Bank  Management System/Profile.Designer.cs
+++ Bank  Management System/Profile.Designer.cs
@@ -67,9 +67,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(254, 212);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.Size = new System.Drawing.Size(46, 16);
             this.label1.TabIndex = 12;
-            this.label1.Text = "Amount";
+            this.label1.Text = "Phone";
             // 
             // textBox2
             // 
