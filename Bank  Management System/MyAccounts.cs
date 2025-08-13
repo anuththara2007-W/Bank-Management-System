32d604bb9251b2d36ff3a01b563f5d8dd12f05ff
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-13 13:05:45 +0530

    Project updated at 2025-08-13 13:05:45

--- Bank  Management System/MyAccounts.cs
+++ Bank  Management System/MyAccounts.cs
@@ -1,20 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace Bank__Management_System
 {
     public partial class MyAccounts : Form
     {
-        public MyAccounts()
+        int customerId;
+        string connString = @"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False";
+
+        public MyAccounts(int cid)
         {
             InitializeComponent();
+            customerId = cid;
+        }
+
+        private void MyAccounts_Load(object sender, EventArgs e)
+        {
+            LoadAccounts();
+        }
+
+        private void LoadAccounts()
+        {
+            using (SqlConnection con = new SqlConnection(connString))
+            {
+                string q = "SELECT Account_ID, Account_Type, Balance, Date_Opened FROM Accounts WHERE Customer_ID = @cid";
+                SqlDataAdapter da = new SqlDataAdapter(q, con);
+                da.SelectCommand.Parameters.AddWithValue("@cid", customerId);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.AutoGenerateColumns = true;
+                dataGridView1.DataSource = dt;
+            }
+        }
+
+        // optionally double-click to show transaction for account
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+            int accountId = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["Account_ID"].Value);
+            var tx = new MyTransactions(customerId, accountId);
+            tx.ShowDialog();
         }
     }
 }
