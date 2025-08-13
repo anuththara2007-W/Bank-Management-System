b1da459714c92a8de6b3d128527199a60bdc7460
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-13 13:17:56 +0530

    Project updated at 2025-08-13 13:17:56

--- Bank  Management System/ChangePassword.cs
+++ Bank  Management System/ChangePassword.cs
@@ -1,34 +1,47 @@
 ﻿using System;
 using System.Data.SqlClient;
 using System.Windows.Forms;
+using static System.Collections.Specialized.BitVector32;
 
-namespace Bank__Management_System
+namespace BankApp
 {
     public partial class ChangePassword : Form
     {
-        int customerId;
         string connString = @"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False";
 
-        public ChangePassword(int cid)
+        public ChangePassword()
         {
             InitializeComponent();
-            customerId = cid;
         }
 
         private void btnChange_Click(object sender, EventArgs e)
         {
-            if (txtNew.Text != txtConfirm.Text) { MessageBox.Show("Passwords do not match."); return; }
+            if (txtNew.Text != txtConfirm.Text)
+            {
+                MessageBox.Show("Passwords do not match");
+                return;
+            }
 
             using (SqlConnection con = new SqlConnection(connString))
             {
                 con.Open();
-                SqlCommand cmd = new SqlCommand("UPDATE Customer SET Password = @pw WHERE Customer_ID = @cid", con);
-                cmd.Parameters.AddWithValue("@pw", txtNew.Text);
-                cmd.Parameters.AddWithValue("@cid", customerId);
-                cmd.ExecuteNonQuery();
+                SqlCommand checkOld = new SqlCommand(
+                    "SELECT COUNT(*) FROM Customer WHERE Customer_ID=@cid AND Password=@old", con);
+                checkOld.Parameters.AddWithValue("@cid", Session.CustomerID);
+                checkOld.Parameters.AddWithValue("@old", txtOld.Text);
+                if ((int)checkOld.ExecuteScalar() == 0)
+                {
+                    MessageBox.Show("Old password incorrect");
+                    return;
+                }
+
+                SqlCommand update = new SqlCommand(
+                    "UPDATE Customer SET Password=@new WHERE Customer_ID=@cid", con);
+                update.Parameters.AddWithValue("@new", txtNew.Text);
+                update.Parameters.AddWithValue("@cid", Session.CustomerID);
+                update.ExecuteNonQuery();
+                MessageBox.Show("Password changed.");
             }
-            MessageBox.Show("Password changed.");
-            this.Close();
         }
     }
 }
