4e69699ae056913727e368b8d2e3c16d4bb886fb
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-13 13:10:54 +0530

    Project updated at 2025-08-13 13:10:54

--- Bank  Management System/ChangePassword.Designer.cs
+++ Bank  Management System/ChangePassword.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ChangePassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "ChangePassword";
             this.Text = "ChangePassword";
+            this.Load += new System.EventHandler(this.ChangePassword_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Bank  Management System/ChangePassword.cs
+++ Bank  Management System/ChangePassword.cs
@@ -1,20 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace Bank__Management_System
 {
     public partial class ChangePassword : Form
     {
-        public ChangePassword()
+        int customerId;
+        string connString = @"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False";
+
+        public ChangePassword(int cid)
         {
             InitializeComponent();
+            customerId = cid;
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            if (txtNew.Text != txtConfirm.Text) { MessageBox.Show("Passwords do not match."); return; }
+
+            using (SqlConnection con = new SqlConnection(connString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE Customer SET Password = @pw WHERE Customer_ID = @cid", con);
+                cmd.Parameters.AddWithValue("@pw", txtNew.Text);
+                cmd.Parameters.AddWithValue("@cid", customerId);
+                cmd.ExecuteNonQuery();
+            }
+            MessageBox.Show("Password changed.");
+            this.Close();
         }
     }
 }
