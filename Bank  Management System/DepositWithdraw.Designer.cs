b6f2e10be67e7fd5a14fd69a49c4ba4f4552ae45
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-13 14:16:18 +0530

    Project updated at 2025-08-13 14:16:18

--- Bank  Management System/DepositWithdraw.Designer.cs
+++ Bank  Management System/DepositWithdraw.Designer.cs
@@ -30,17 +30,29 @@ namespace BankApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblAccount = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblAccount
+            // 
+            this.lblAccount.AutoSize = true;
+            this.lblAccount.Location = new System.Drawing.Point(42, 36);
+            this.lblAccount.Name = "lblAccount";
+            this.lblAccount.Size = new System.Drawing.Size(44, 16);
+            this.lblAccount.TabIndex = 0;
+            this.lblAccount.Text = "label1";
             // 
             // DepositWithdraw
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(985, 577);
+            this.Controls.Add(this.lblAccount);
             this.Name = "DepositWithdraw";
             this.Text = "DepositWithdraw";
             this.Load += new System.EventHandler(this.DepositWithdraw_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -50,5 +62,7 @@ namespace BankApp
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblAccount;
     }
 }
