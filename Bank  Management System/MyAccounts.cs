00198f88a113778fbb8893f2737c0e1ed82253bd
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-16 20:04:01 +0530

    Auto-update

--- Bank  Management System/MyAccounts.cs
+++ Bank  Management System/MyAccounts.cs
@@ -36,7 +36,7 @@ namespace BankApp
             }
         }
 
-        private void btnGoBack_Click(object sender, EventArgs e)
+        private object btnGoBack_Click(object sender, EventArgs e)
         {
             CustomerDashboard customerdash = new CustomerDashboard();
             customerdash.Show();
