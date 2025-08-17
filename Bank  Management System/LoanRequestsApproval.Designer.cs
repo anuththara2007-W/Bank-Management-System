70f58145ce7c44916ca877f3bdc8a0d40f12846a
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-17 18:56:20 +0530

    Auto-update

--- Bank  Management System/LoanRequestsApproval.Designer.cs
+++ Bank  Management System/LoanRequestsApproval.Designer.cs
@@ -31,6 +31,8 @@ namespace BankApp
         private void InitializeComponent()
         {
             this.dgvAllRequests = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAllRequests)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,9 +46,29 @@ namespace BankApp
             this.dgvAllRequests.Size = new System.Drawing.Size(1109, 183);
             this.dgvAllRequests.TabIndex = 0;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(188, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(730, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(232, 58);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // AdminLoanRequests
             // 
             this.ClientSize = new System.Drawing.Size(1170, 570);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dgvAllRequests);
             this.Name = "AdminLoanRequests";
             ((System.ComponentModel.ISupportInitialize)(this.dgvAllRequests)).EndInit();
@@ -62,5 +84,7 @@ namespace BankApp
 
         private System.Windows.Forms.DataGridView dgvRequests;
         private System.Windows.Forms.DataGridView dgvAllRequests;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
