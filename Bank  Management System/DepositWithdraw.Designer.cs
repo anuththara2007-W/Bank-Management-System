b36f860359704418c8b2d41daadf17d6b906bb8d
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-17 19:11:05 +0530

    Auto-update

--- Bank  Management System/DepositWithdraw.Designer.cs
+++ Bank  Management System/DepositWithdraw.Designer.cs
@@ -99,6 +99,7 @@ namespace BankApp
             this.button1.TabIndex = 16;
             this.button1.Text = "Go Back";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // lblMode
             // 
