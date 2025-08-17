99c6ba0b9536d71f3040da8b1959105f9d5edc5d
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-17 17:57:26 +0530

    Auto-update

--- Bank  Management System/LoanRequestsApproval.Designer.cs
+++ Bank  Management System/LoanRequestsApproval.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // LoanRequestsApproval
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "LoanRequestsApproval";
             this.Text = "LoanRequestsApproval";
+            this.Load += new System.EventHandler(this.LoanRequestsApproval_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Bank  Management System/LoanRequestsApproval.cs
+++ Bank  Management System/LoanRequestsApproval.cs
@@ -10,11 +10,89 @@ using System.Windows.Forms;
 
 namespace Bank__Management_System
 {
-    public partial class LoanRequestsApproval : Form
+    using System;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Windows.Forms;
+
+    namespace BankApp
     {
-        public LoanRequestsApproval()
+        public partial class LoanRequestsApproval : Form
         {
-            InitializeComponent();
+            string connString = @"Data Source=(localdb)\Local;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False";
+
+            public LoanRequestsApproval()
+            {
+                InitializeComponent();
+                LoadRequests();
+            }
+
+            private void LoadRequests()
+            {
+                using (SqlConnection con = new SqlConnection(connString))
+                {
+                    SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM LoanRequests WHERE Status='Pending'", con);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dgvRequests.DataSource = dt; // Show in DataGridView
+                }
+            }
+
+            private void btnApprove_Click(object sender, EventArgs e)
+            {
+                if (dgvRequests.SelectedRows.Count > 0)
+                {
+                    int requestId = Convert.ToInt32(dgvRequests.SelectedRows[0].Cells["RequestID"].Value);
+                    int customerId = Convert.ToInt32(dgvRequests.SelectedRows[0].Cells["Customer_ID"].Value);
+                    string loanType = dgvRequests.SelectedRows[0].Cells["LoanType"].Value.ToString();
+                    decimal amount = Convert.ToDecimal(dgvRequests.SelectedRows[0].Cells["Amount"].Value);
+
+                    using (SqlConnection con = new SqlConnection(connString))
+                    {
+                        con.Open();
+
+                        // Insert into Loans table
+                        SqlCommand insertLoan = new SqlCommand(
+                            "INSERT INTO Loans (Customer_ID, LoanType, Amount, InterestRate, LoanDate) " +
+                            "VALUES (@cid, @type, @amt, @rate, GETDATE())", con);
+
+                        insertLoan.Parameters.AddWithValue("@cid", customerId);
+                        insertLoan.Parameters.AddWithValue("@type", loanType);
+                        insertLoan.Parameters.AddWithValue("@amt", amount);
+                        insertLoan.Parameters.AddWithValue("@rate", 5.5m);
+
+                        insertLoan.ExecuteNonQuery();
+
+                        // Update request status
+                        SqlCommand updateRequest = new SqlCommand(
+                            "UPDATE LoanRequests SET Status='Approved' WHERE RequestID=@rid", con);
+                        updateRequest.Parameters.AddWithValue("@rid", requestId);
+                        updateRequest.ExecuteNonQuery();
+
+                        MessageBox.Show("✅ Loan approved and added to Loans table!");
+                        LoadRequests();
+                    }
+                }
+            }
+
+            private void btnReject_Click(object sender, EventArgs e)
+            {
+                if (dgvRequests.SelectedRows.Count > 0)
+                {
+                    int requestId = Convert.ToInt32(dgvRequests.SelectedRows[0].Cells["RequestID"].Value);
+
+                    using (SqlConnection con = new SqlConnection(connString))
+                    {
+                        con.Open();
+                        SqlCommand cmd = new SqlCommand(
+                            "UPDATE LoanRequests SET Status='Rejected' WHERE RequestID=@rid", con);
+                        cmd.Parameters.AddWithValue("@rid", requestId);
+                        cmd.ExecuteNonQuery();
+
+                        MessageBox.Show("❌ Loan request rejected.");
+                        LoadRequests();
+                    }
+                }
+            }
         }
     }
-}
