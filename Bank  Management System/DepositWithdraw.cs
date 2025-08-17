c9ce3ca29ec40c561cc1376c1b0626603625d5f7
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-17 20:20:35 +0530

    Project Updated

--- Bank  Management System/DepositWithdraw.cs
+++ Bank  Management System/DepositWithdraw.cs
@@ -92,8 +92,8 @@ namespace Bank__Management_System
                 try
                 {
                     string sql = (mode == "deposit")
-                        ? "UPDATE Accounts SET Balance = Balance + @amt WHERE Account_ID = @aid"
-                        : "UPDATE Accounts SET Balance = Balance - @amt WHERE Account_ID = @aid AND Balance >= @amt";
+                        ? "UPDATE accounts SET Balance = Balance + @amt WHERE Account_ID = @aid"
+                        : "UPDATE accounts SET Balance = Balance - @amt WHERE Account_ID = @aid AND Balance >= @amt";
 
                     SqlCommand cmd = new SqlCommand(sql, con, trans);
                     cmd.Parameters.AddWithValue("@amt", amount);
