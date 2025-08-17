c4498ee7803f3e0520ecff2a1789711f727ec511
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-17 20:29:10 +0530

    Project Updated

--- Bank  Management System/Account.Designer.cs
+++ Bank  Management System/Account.Designer.cs
@@ -40,12 +40,10 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnAdd = new System.Windows.Forms.Button();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.btnSearch = new System.Windows.Forms.PictureBox();
-            this.txtAccountID = new System.Windows.Forms.TextBox();
             this.txtSearch = new System.Windows.Forms.TextBox();
             this.txtCustomerID = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
@@ -135,7 +133,7 @@
             this.txtAccountType.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtAccountType.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.txtAccountType.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtAccountType.Location = new System.Drawing.Point(827, 280);
+            this.txtAccountType.Location = new System.Drawing.Point(827, 232);
             this.txtAccountType.Name = "txtAccountType";
             this.txtAccountType.Size = new System.Drawing.Size(319, 27);
             this.txtAccountType.TabIndex = 8;
@@ -145,7 +143,7 @@
             this.txtBalance.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtBalance.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.txtBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBalance.Location = new System.Drawing.Point(827, 333);
+            this.txtBalance.Location = new System.Drawing.Point(827, 285);
             this.txtBalance.Name = "txtBalance";
             this.txtBalance.Size = new System.Drawing.Size(319, 27);
             this.txtBalance.TabIndex = 9;
@@ -155,7 +153,7 @@
             this.txtname.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtname.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.txtname.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtname.Location = new System.Drawing.Point(827, 437);
+            this.txtname.Location = new System.Drawing.Point(827, 389);
             this.txtname.Name = "txtname";
             this.txtname.Size = new System.Drawing.Size(319, 27);
             this.txtname.TabIndex = 11;
@@ -166,7 +164,7 @@
             this.txtCustomerName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.txtCustomerName.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtCustomerName.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.txtCustomerName.Location = new System.Drawing.Point(617, 441);
+            this.txtCustomerName.Location = new System.Drawing.Point(617, 393);
             this.txtCustomerName.Name = "txtCustomerName";
             this.txtCustomerName.Size = new System.Drawing.Size(153, 25);
             this.txtCustomerName.TabIndex = 17;
@@ -178,7 +176,7 @@
             this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(617, 279);
+            this.label4.Location = new System.Drawing.Point(617, 231);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(131, 25);
             this.label4.TabIndex = 16;
@@ -190,7 +188,7 @@
             this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(617, 333);
+            this.label3.Location = new System.Drawing.Point(617, 285);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(78, 25);
             this.label3.TabIndex = 15;
@@ -202,23 +200,11 @@
             this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(617, 389);
+            this.label2.Location = new System.Drawing.Point(617, 341);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(128, 25);
             this.label2.TabIndex = 14;
             this.label2.Text = "Date Opened";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(617, 227);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 25);
-            this.label1.TabIndex = 13;
-            this.label1.Text = "Account ID";
             // 
             // dataGridView1
             // 
@@ -256,7 +242,7 @@
             // 
             this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(827, 389);
+            this.dateTimePicker1.Location = new System.Drawing.Point(827, 341);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(312, 22);
             this.dateTimePicker1.TabIndex = 19;
@@ -274,16 +260,6 @@
             this.btnSearch.TabIndex = 20;
             this.btnSearch.TabStop = false;
             this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
-            // 
-            // txtAccountID
-            // 
-            this.txtAccountID.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtAccountID.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.txtAccountID.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtAccountID.Location = new System.Drawing.Point(827, 224);
-            this.txtAccountID.Name = "txtAccountID";
-            this.txtAccountID.Size = new System.Drawing.Size(319, 27);
-            this.txtAccountID.TabIndex = 7;
             // 
             // txtSearch
             // 
@@ -304,7 +280,7 @@
             this.txtCustomerID.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtCustomerID.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.txtCustomerID.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCustomerID.Location = new System.Drawing.Point(808, 485);
+            this.txtCustomerID.Location = new System.Drawing.Point(827, 441);
             this.txtCustomerID.Name = "txtCustomerID";
             this.txtCustomerID.Size = new System.Drawing.Size(319, 27);
             this.txtCustomerID.TabIndex = 22;
@@ -315,7 +291,7 @@
             this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label5.Location = new System.Drawing.Point(583, 520);
+            this.label5.Location = new System.Drawing.Point(607, 443);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(120, 25);
             this.label5.TabIndex = 23;
@@ -331,7 +307,7 @@
             this.btnPickCustomer.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(233)))), ((int)(((byte)(236)))));
             this.btnPickCustomer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnPickCustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPickCustomer.Location = new System.Drawing.Point(533, 475);
+            this.btnPickCustomer.Location = new System.Drawing.Point(524, 475);
             this.btnPickCustomer.Name = "btnPickCustomer";
             this.btnPickCustomer.Size = new System.Drawing.Size(269, 37);
             this.btnPickCustomer.TabIndex = 29;
@@ -357,11 +333,9 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtname);
             this.Controls.Add(this.txtBalance);
             this.Controls.Add(this.txtAccountType);
-            this.Controls.Add(this.txtAccountID);
             this.Controls.Add(this.btnGoBack);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.btnDelete);
@@ -392,12 +366,10 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.PictureBox btnSearch;
-        private System.Windows.Forms.TextBox txtAccountID;
         private System.Windows.Forms.TextBox txtSearch;
         private System.Windows.Forms.TextBox txtCustomerID;
         private System.Windows.Forms.Label label5;
