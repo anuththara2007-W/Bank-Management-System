78bc8f59876cf50db19fa1bbe7d50951b99abc63
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-22 21:51:07 +0530

    Project Updated

--- Bank  Management System/DepositWithdraw.Designer.cs
+++ Bank  Management System/DepositWithdraw.Designer.cs
@@ -74,7 +74,6 @@ namespace Bank__Management_System
             this.btnDeposit.TabIndex = 4;
             this.btnDeposit.Text = "Deposit ";
             this.btnDeposit.UseVisualStyleBackColor = true;
-            this.btnDeposit.Click += new System.EventHandler(this.btnDeposit_Click);
             // 
             // btnWithdraw
             // 
@@ -84,7 +83,6 @@ namespace Bank__Management_System
             this.btnWithdraw.TabIndex = 5;
             this.btnWithdraw.Text = "Withdraw";
             this.btnWithdraw.UseVisualStyleBackColor = true;
-            this.btnWithdraw.Click += new System.EventHandler(this.btnWithdraw_Click);
             // 
             // button1
             // 
