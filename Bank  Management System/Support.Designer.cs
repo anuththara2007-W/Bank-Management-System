315d19055d316c559225c8f6b9edc8c071a3701e
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-08-31 14:53:52 +0530

    Project Updated

--- Bank  Management System/Support.Designer.cs
+++ Bank  Management System/Support.Designer.cs
@@ -71,7 +71,6 @@ namespace BankApp
             this.button1.TabIndex = 29;
             this.button1.Text = "Go Back";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // dgvMyTickets
             // 
