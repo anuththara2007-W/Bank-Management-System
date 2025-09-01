84c69ee08be5d4e6281a4aebcf93070e0a32915b
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-01 17:47:12 +0530

    Project Updated

--- Bank  Management System/Contact.cs
+++ Bank  Management System/Contact.cs
@@ -44,6 +44,8 @@ namespace Bank__Management_System
         private void lblHome_Click(object sender, EventArgs e)
         {
             Landing landing = new Landing();
+            landing.Show();
+            this
         }
     }
 }
