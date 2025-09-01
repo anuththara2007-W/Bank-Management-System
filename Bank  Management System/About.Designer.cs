c072999aa18069ffb277e8ecf3e12efde45d6853
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-01 15:27:17 +0530

    Project Updated

--- Bank  Management System/About.Designer.cs
+++ Bank  Management System/About.Designer.cs
@@ -35,7 +35,7 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.lblAbout = new System.Windows.Forms.Label();
             this.lblcontact1 = new System.Windows.Forms.Label();
             this.lblHome = new System.Windows.Forms.Label();
             this.lblcontact2 = new System.Windows.Forms.Label();
@@ -113,19 +113,19 @@
             this.label7.TabIndex = 6;
             this.label7.Text = "Your data is protected with advanced encryption.\n";
             // 
-            // label3
+            // lblAbout
             // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
-            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label3.Location = new System.Drawing.Point(354, 56);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 19);
-            this.label3.TabIndex = 12;
-            this.label3.Text = "ABOUT  US ";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblAbout.AutoSize = true;
+            this.lblAbout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.lblAbout.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblAbout.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAbout.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblAbout.Location = new System.Drawing.Point(354, 56);
+            this.lblAbout.Name = "lblAbout";
+            this.lblAbout.Size = new System.Drawing.Size(87, 19);
+            this.lblAbout.TabIndex = 12;
+            this.lblAbout.Text = "ABOUT  US ";
+            this.lblAbout.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // lblcontact1
             // 
@@ -177,7 +177,7 @@
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1280, 785);
             this.Controls.Add(this.lblcontact2);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblAbout);
             this.Controls.Add(this.lblcontact1);
             this.Controls.Add(this.lblHome);
             this.Controls.Add(this.label7);
@@ -203,7 +203,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblAbout;
         private System.Windows.Forms.Label lblcontact1;
         private System.Windows.Forms.Label lblHome;
         private System.Windows.Forms.Label lblcontact2;
