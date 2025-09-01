860100f1bd49e5e2256b896a817638e84038b336
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-01 15:02:02 +0530

    Project Updated

--- Bank  Management System/About.Designer.cs
+++ Bank  Management System/About.Designer.cs
@@ -30,19 +30,45 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
-            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label1.Location = new System.Drawing.Point(581, 165);
+            this.label1.Location = new System.Drawing.Point(647, 169);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(600, 23);
+            this.label1.Size = new System.Drawing.Size(575, 22);
             this.label1.TabIndex = 1;
             this.label1.Text = "BankApp makes managing your money effortless. Check balances, transfer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(353, 381);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(575, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "BankApp makes managing your money effortless. Check balances, transfer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(660, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(593, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "funds, pay bills, and track transactions anytime, anywhere. With secure login,";
             // 
             // About
             // 
@@ -51,6 +77,8 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1280, 785);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "About";
             this.Text = "About";
@@ -62,5 +90,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
