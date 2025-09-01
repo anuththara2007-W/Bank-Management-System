db0b24b2b91a7979590dea6accbf0b2bccbf710d
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-01 15:58:07 +0530

    Project Updated

--- Bank  Management System/About.cs
+++ Bank  Management System/About.cs
@@ -37,7 +37,8 @@ namespace Bank__Management_System
 
         private void lblcontact2_Click(object sender, EventArgs e)
         {
-          
+            string url = "https://wa.me/94707266991";
+            System.Diagnostics.Process.Start(url);
         }
     }
 }
