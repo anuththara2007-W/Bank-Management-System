2e36ad131aa097ac2ee527ce9b6d592a9f4b67b4
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-01 17:30:25 +0530

    Project Updated

--- Bank  Management System/Contact.Designer.cs
+++ Bank  Management System/Contact.Designer.cs
@@ -32,6 +32,7 @@
             this.lblAbout = new System.Windows.Forms.Label();
             this.lblcontact1 = new System.Windows.Forms.Label();
             this.lblHome = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblAbout
@@ -39,9 +40,9 @@
             this.lblAbout.AutoSize = true;
             this.lblAbout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
             this.lblAbout.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.lblAbout.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAbout.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblAbout.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblAbout.Location = new System.Drawing.Point(861, 69);
+            this.lblAbout.Location = new System.Drawing.Point(570, 41);
             this.lblAbout.Name = "lblAbout";
             this.lblAbout.Size = new System.Drawing.Size(87, 19);
             this.lblAbout.TabIndex = 15;
@@ -53,11 +54,11 @@
             this.lblcontact1.AutoSize = true;
             this.lblcontact1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
             this.lblcontact1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.lblcontact1.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcontact1.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblcontact1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblcontact1.Location = new System.Drawing.Point(1040, 70);
+            this.lblcontact1.Location = new System.Drawing.Point(799, 40);
             this.lblcontact1.Name = "lblcontact1";
-            this.lblcontact1.Size = new System.Drawing.Size(75, 19);
+            this.lblcontact1.Size = new System.Drawing.Size(90, 22);
             this.lblcontact1.TabIndex = 14;
             this.lblcontact1.Text = "CONTACT";
             this.lblcontact1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -69,12 +70,26 @@
             this.lblHome.Cursor = System.Windows.Forms.Cursors.Hand;
             this.lblHome.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblHome.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblHome.Location = new System.Drawing.Point(735, 70);
+            this.lblHome.Location = new System.Drawing.Point(351, 41);
             this.lblHome.Name = "lblHome";
             this.lblHome.Size = new System.Drawing.Size(60, 19);
             this.lblHome.TabIndex = 13;
             this.lblHome.Text = "HOME  ";
             this.lblHome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(1120, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 30);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "CONTACT";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Contact
             // 
@@ -83,6 +98,7 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1280, 785);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblAbout);
             this.Controls.Add(this.lblcontact1);
             this.Controls.Add(this.lblHome);
@@ -101,5 +117,6 @@
         private System.Windows.Forms.Label lblAbout;
         private System.Windows.Forms.Label lblcontact1;
         private System.Windows.Forms.Label lblHome;
+        private System.Windows.Forms.Label label1;
     }
 }
