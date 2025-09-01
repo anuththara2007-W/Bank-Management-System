1d0ab56749b6132ec7b8abdd18d7bdee1c559fa8
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-01 15:05:54 +0530

    Project Updated

--- Bank  Management System/About.Designer.cs
+++ Bank  Management System/About.Designer.cs
@@ -42,11 +42,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
-            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label1.Location = new System.Drawing.Point(673, 175);
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(626, 171);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(498, 19);
+            this.label1.Size = new System.Drawing.Size(575, 22);
             this.label1.TabIndex = 1;
             this.label1.Text = "BankApp makes managing your money effortless. Check balances, transfer";
             // 
@@ -54,11 +54,11 @@
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
-            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label2.Location = new System.Drawing.Point(673, 219);
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(626, 219);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(485, 19);
+            this.label2.Size = new System.Drawing.Size(563, 22);
             this.label2.TabIndex = 2;
             this.label2.Text = "instant notifications, and intuitive design, banking has never been easier.";
             // 
@@ -66,23 +66,23 @@
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
-            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label3.Location = new System.Drawing.Point(686, 197);
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(639, 195);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(510, 19);
+            this.label3.Size = new System.Drawing.Size(589, 22);
             this.label3.TabIndex = 3;
-            this.label3.Text = "funds, pay bills, and track transactions anytime, anywhere. With secure login,";
+            this.label3.Text = "funds, pay bills, and track transactions anytime, anywhere. With secure login";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
-            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label4.Location = new System.Drawing.Point(673, 197);
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(626, 195);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(510, 19);
+            this.label4.Size = new System.Drawing.Size(593, 22);
             this.label4.TabIndex = 3;
             this.label4.Text = "funds, pay bills, and track transactions anytime, anywhere. With secure login,";
             // 
@@ -90,11 +90,11 @@
             // 
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
-            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label5.Location = new System.Drawing.Point(398, 383);
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(634, 483);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(357, 19);
+            this.label5.Size = new System.Drawing.Size(411, 22);
             this.label5.TabIndex = 4;
             this.label5.Text = " All transactions are safe and monitored in real-time.";
             // 
@@ -102,11 +102,11 @@
             // 
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
-            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label6.Location = new System.Drawing.Point(681, 463);
+            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(634, 461);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(346, 19);
+            this.label6.Size = new System.Drawing.Size(404, 22);
             this.label6.TabIndex = 5;
             this.label6.Text = " Secure login ensures only you access your account.\n";
             // 
@@ -114,11 +114,11 @@
             // 
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
-            this.label7.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label7.Location = new System.Drawing.Point(686, 442);
+            this.label7.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.Location = new System.Drawing.Point(639, 440);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(333, 19);
+            this.label7.Size = new System.Drawing.Size(384, 22);
             this.label7.TabIndex = 6;
             this.label7.Text = "Your data is protected with advanced encryption.\n";
             // 
