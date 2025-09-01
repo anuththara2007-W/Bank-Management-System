9b2e3ca55fd7fe3cca0b3f2024d1973aea917455
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-01 16:02:58 +0530

    Project Updated

--- Bank  Management System/Contact.Designer.cs
+++ Bank  Management System/Contact.Designer.cs
@@ -29,7 +29,52 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Contact));
+            this.lblAbout = new System.Windows.Forms.Label();
+            this.lblcontact1 = new System.Windows.Forms.Label();
+            this.lblHome = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblAbout
+            // 
+            this.lblAbout.AutoSize = true;
+            this.lblAbout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.lblAbout.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblAbout.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAbout.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblAbout.Location = new System.Drawing.Point(861, 69);
+            this.lblAbout.Name = "lblAbout";
+            this.lblAbout.Size = new System.Drawing.Size(87, 19);
+            this.lblAbout.TabIndex = 15;
+            this.lblAbout.Text = "ABOUT  US ";
+            this.lblAbout.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblcontact1
+            // 
+            this.lblcontact1.AutoSize = true;
+            this.lblcontact1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.lblcontact1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblcontact1.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcontact1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblcontact1.Location = new System.Drawing.Point(1040, 70);
+            this.lblcontact1.Name = "lblcontact1";
+            this.lblcontact1.Size = new System.Drawing.Size(75, 19);
+            this.lblcontact1.TabIndex = 14;
+            this.lblcontact1.Text = "CONTACT";
+            this.lblcontact1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblHome
+            // 
+            this.lblHome.AutoSize = true;
+            this.lblHome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.lblHome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblHome.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHome.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblHome.Location = new System.Drawing.Point(735, 70);
+            this.lblHome.Name = "lblHome";
+            this.lblHome.Size = new System.Drawing.Size(60, 19);
+            this.lblHome.TabIndex = 13;
+            this.lblHome.Text = "HOME  ";
+            this.lblHome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Contact
             // 
@@ -38,15 +83,23 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1280, 785);
+            this.Controls.Add(this.lblAbout);
+            this.Controls.Add(this.lblcontact1);
+            this.Controls.Add(this.lblHome);
             this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Contact";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Contact";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblAbout;
+        private System.Windows.Forms.Label lblcontact1;
+        private System.Windows.Forms.Label lblHome;
     }
 }
