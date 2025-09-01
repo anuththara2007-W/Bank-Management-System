a57e10db45edd6abaea6eaaf109c311b39c056c2
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-01 15:59:52 +0530

    Project Updated

--- Bank  Management System/Contact.Designer.cs
+++ Bank  Management System/Contact.Designer.cs
@@ -35,9 +35,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1280, 785);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Contact";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Contact";
             this.ResumeLayout(false);
 
