5ae952128d1f4d6ec28b332f64754b1e7aef12dc
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-12 22:00:46 +0530

    Project Updated

--- ManageBot.Designer.cs
+++ ManageBot.Designer.cs
@@ -37,7 +37,7 @@
             this.webView21.AllowExternalDrop = true;
             this.webView21.CreationProperties = null;
             this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
-            this.webView21.Location = new System.Drawing.Point(1, 1);
+            this.webView21.Location = new System.Drawing.Point(-10, 50);
             this.webView21.Name = "webView21";
             this.webView21.Size = new System.Drawing.Size(1059, 605);
             this.webView21.TabIndex = 0;
