5a8acde63f7726e603ccaa455e8024d17f0ecac6
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-12 22:08:23 +0530

    Project Updated

--- ChatBot.cs
+++ ChatBot.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using Microsoft.Web.WebView2.WinForms;
 using Microsoft.Web.WebView2.Core;
 
 namespace Bank__Management_System
@@ -14,16 +15,14 @@ namespace Bank__Management_System
 
         private async void InitializeAsync()
         {
-            // Make sure webView21 exists (added via Designer)
             await webView21.EnsureCoreWebView2Async(null);
             webView21.CoreWebView2.Navigate("https://app.fastbots.ai/embed/cmfgy9k0900w5qp1krjn4p7ex");
         }
 
         private void btnRefresh_Click(object sender, EventArgs e)
         {
-            ChatBot chatbot = new ChatBot();    
-            chatbot.Show();
-            this.Hide();
+            // Simply reload the current page
+            webView21.Reload();
         }
     }
 }
