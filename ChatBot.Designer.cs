3c735a1f3bef155c08a2462861c47532e90a4ff2
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-12 21:33:23 +0530

    Project Updated

--- ChatBot.Designer.cs
+++ ChatBot.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // webBrowser1
@@ -41,32 +40,20 @@
             this.webBrowser1.Size = new System.Drawing.Size(1145, 605);
             this.webBrowser1.TabIndex = 0;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 553);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            // 
             // ChatBot
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1145, 605);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.webBrowser1);
             this.Name = "ChatBot";
             this.Text = "ChatBot";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.WebBrowser webBrowser1;
-        private System.Windows.Forms.Label label1;
     }
 }
