551f8316c8cf18410df716874e3079cfd669879c
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-12 22:10:09 +0530

    Project Updated

--- ManageBot.cs
+++ ManageBot.cs
@@ -23,5 +23,10 @@ namespace Bank__Management_System
             await webView21.EnsureCoreWebView2Async(null);
             webView21.CoreWebView2.Navigate("https://app.fastbots.ai/bots/cmfgy9k0900w5qp1krjn4p7ex/history?page=1&limit=30");
         }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            webView21.Reload();
+        }
     }
 }
