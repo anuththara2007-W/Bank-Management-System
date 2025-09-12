11ec91242c53ebcceb1cd8dbe958992d77bc9899
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-12 21:55:06 +0530

    Project Updated

--- ManageBot.cs
+++ ManageBot.cs
@@ -14,6 +14,11 @@ namespace Bank__Management_System
     {
         public ManageBot()
         {
+            InitializeComponent();
+        }
+        private async void InitializeAsync()
+        {
+            // Make sure webView21 exists (added via Designer)
             await webView21.EnsureCoreWebView2Async(null);
             webView21.CoreWebView2.Navigate("https://app.fastbots.ai/embed/cmfgy9k0900w5qp1krjn4p7ex");
         }
