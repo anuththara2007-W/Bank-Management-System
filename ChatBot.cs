a8cfa8624780f650d8da871448af6b1374d49b68
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-12 21:46:14 +0530

    Project Updated

--- ChatBot.cs
+++ ChatBot.cs
@@ -1,20 +1,32 @@
 ﻿using System;
 using System.Windows.Forms;
+using Microsoft.Web.WebView2.WinForms;
 using Microsoft.Web.WebView2.Core;
 
 namespace Bank__Management_System
 {
     public partial class ChatBot : Form
     {
+        private WebView2 webView21;
+
         public ChatBot()
         {
             InitializeComponent();
+            InitializeWebView();
             InitializeAsync();
+        }
+
+        private void InitializeWebView()
+        {
+            webView21 = new WebView2
+            {
+                Dock = DockStyle.Fill
+            };
+            this.Controls.Add(webView21);
         }
 
         private async void InitializeAsync()
         {
-            // Make sure webView21 exists (added via Designer)
             await webView21.EnsureCoreWebView2Async(null);
             webView21.CoreWebView2.Navigate("https://your-chatbot-website.com");
         }
