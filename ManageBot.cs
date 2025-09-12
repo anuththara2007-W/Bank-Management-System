0d9b1bd3b87d97611dd6bdf67986048bc37ad08b
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-12 21:52:31 +0530

    Project Updated

--- ManageBot.cs
+++ ManageBot.cs
@@ -1,32 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.Web.WebView2.WinForms;
-using Microsoft.Web.WebView2.Core;
 
 namespace Bank__Management_System
 {
     public partial class ManageBot : Form
     {
-        private WebView2 webView21;
-
         public ManageBot()
         {
             InitializeComponent();
-            InitializeWebView();
-        }
-
-        private async void InitializeWebView()
-        {
-            // Create WebView2 and fill the form
-            webView21 = new WebView2
-            {
-                Dock = DockStyle.Fill
-            };
-            this.Controls.Add(webView21);
-
-            // Initialize and navigate to chatbot
-            await webView21.EnsureCoreWebView2Async(null);
-            webView21.CoreWebView2.Navigate("https://app.fastbots.ai/embed/cmfgy9k0900w5qp1krjn4p7ex");
         }
     }
 }
