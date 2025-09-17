079a234acadd829646de9462ad1b4757e3c3a5a0
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-17 16:07:02 +0530

    Project Updated

--- LoanRequest.Designer.cs
+++ LoanRequest.Designer.cs
@@ -43,9 +43,10 @@ namespace BankApp
             // 
             // btnSubmitLoan
             // 
-            this.btnSubmitLoan.Location = new System.Drawing.Point(687, 386);
+            this.btnSubmitLoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmitLoan.Location = new System.Drawing.Point(666, 386);
             this.btnSubmitLoan.Name = "btnSubmitLoan";
-            this.btnSubmitLoan.Size = new System.Drawing.Size(189, 30);
+            this.btnSubmitLoan.Size = new System.Drawing.Size(228, 47);
             this.btnSubmitLoan.TabIndex = 10;
             this.btnSubmitLoan.Text = "Submit";
             this.btnSubmitLoan.UseVisualStyleBackColor = true;
