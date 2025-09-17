dc48951cb099f93c5ef0c06519f7e54b2b488b4e
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-17 16:07:13 +0530

    Project Updated

--- LoanRequest.Designer.cs
+++ LoanRequest.Designer.cs
@@ -87,6 +87,7 @@ namespace BankApp
             this.btnGoBack.FlatAppearance.BorderSize = 0;
             this.btnGoBack.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
             this.btnGoBack.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnGoBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnGoBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnGoBack.Location = new System.Drawing.Point(402, 386);
             this.btnGoBack.Name = "btnGoBack";
