e337e0c769706947acace80de871d29b533c9887
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-17 15:37:52 +0530

    Project Updated

--- ManageBot.Designer.cs
+++ ManageBot.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ManageBot));
             this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
             this.btnRefreshs = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnRefreshs)).BeginInit();
             this.SuspendLayout();
@@ -58,11 +59,21 @@
             this.btnRefreshs.TabStop = false;
             this.btnRefreshs.Click += new System.EventHandler(this.btnRefreshs_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(936, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
             // ManageBot
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1280, 785);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnRefreshs);
             this.Controls.Add(this.webView21);
             this.Name = "ManageBot";
@@ -71,6 +82,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnRefreshs)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -78,5 +90,6 @@
 
         private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
         private System.Windows.Forms.PictureBox btnRefreshs;
+        private System.Windows.Forms.Label label1;
     }
 }
