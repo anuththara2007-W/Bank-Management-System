0b5e98f578201ea70794ea75f74ef86f34016073
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-17 16:44:51 +0530

    Project Updated

--- GridStyle.cs
+++ GridStyle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,36 @@ namespace Bank__Management_System
     {
         public static void ModernizeGrid(DataGridView grid)
         {
+
+            grid.BorderStyle = BorderStyle.None;
+            grid.BackgroundColor = Color.White;
+            grid.EnableHeadersVisualStyles = false;
+            grid.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+
+            // Header
+            grid.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            grid.ColumnHeadersDefaultCellStyle.BackColor = Color.White;
+            grid.ColumnHeadersDefaultCellStyle.ForeColor = Color.Black;
+            grid.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            grid.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+
+            // Rows
+            grid.DefaultCellStyle.BackColor = Color.White;
+            grid.DefaultCellStyle.ForeColor = Color.Black;
+            grid.DefaultCellStyle.Font = new Font("Segoe UI", 10);
+            grid.DefaultCellStyle.SelectionBackColor = Color.FromArgb(240, 248, 255);
+            grid.DefaultCellStyle.SelectionForeColor = Color.Black;
+
+            // Alternate row shading
+            grid.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(250, 250, 250);
+
+            // No row header
+            grid.RowHeadersVisible = false;
+            grid.RowTemplate.Height = 35;
+            foreach (DataGridViewRow row in dgvLoanRequests.Rows)
+            {
+
+            }
         }
     }
 }
