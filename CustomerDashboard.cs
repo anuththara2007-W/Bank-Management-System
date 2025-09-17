0ac7ba6e3668ae46ec51e4699a2c5918b8ab0ab8
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-17 19:18:30 +0530

    Project Updated

--- CustomerDashboard.cs
+++ CustomerDashboard.cs
@@ -20,6 +20,8 @@ namespace BankApp
             LoadBalance();
             LoadRecentTransactions();
             LoadLoanSummary();
+            dgvLoanRequests.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            GridStyle.ModernizeGrid(dgvLoanRequests);
         }
 
         private void LoadBalance()
