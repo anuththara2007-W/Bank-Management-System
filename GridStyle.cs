b831d829752a0cfb8aafb64cd30fd1caa1ad6892
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-17 16:44:34 +0530

    Project Updated

--- GridStyle.cs
+++ GridStyle.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Bank__Management_System
 {
@@ -10,7 +11,6 @@ namespace Bank__Management_System
     {
         public static void ModernizeGrid(DataGridView grid)
         {
-            // your styling code here
         }
     }
 }
