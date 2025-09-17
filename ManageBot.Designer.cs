459ffa43e57e2abc412680bb4faf0a87af01049e
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-17 15:38:25 +0530

    Project Updated

--- ManageBot.Designer.cs
+++ ManageBot.Designer.cs
@@ -31,7 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ManageBot));
             this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
             this.btnRefreshs = new System.Windows.Forms.PictureBox();
-            this.lblGoBack = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnRefreshs)).BeginInit();
             this.SuspendLayout();
@@ -59,21 +59,21 @@
             this.btnRefreshs.TabStop = false;
             this.btnRefreshs.Click += new System.EventHandler(this.btnRefreshs_Click);
             // 
-            // lblGoBack
+            // label1
             // 
-            this.lblGoBack.AutoSize = true;
-            this.lblGoBack.Location = new System.Drawing.Point(936, 30);
-            this.lblGoBack.Name = "lblGoBack";
-            this.lblGoBack.Size = new System.Drawing.Size(44, 16);
-            this.lblGoBack.TabIndex = 5;
-            this.lblGoBack.Text = "label1";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(936, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Go Back";
             // 
             // ManageBot
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1280, 785);
-            this.Controls.Add(this.lblGoBack);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnRefreshs);
             this.Controls.Add(this.webView21);
             this.Name = "ManageBot";
@@ -90,6 +90,6 @@
 
         private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
         private System.Windows.Forms.PictureBox btnRefreshs;
-        private System.Windows.Forms.Label lblGoBack;
+        private System.Windows.Forms.Label label1;
     }
 }
