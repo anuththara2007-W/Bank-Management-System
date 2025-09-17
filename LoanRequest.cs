c1899ce783fc273b46d0a7523fbc952ba57062f3
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-17 16:11:31 +0530

    Project Updated

--- LoanRequest.cs
+++ LoanRequest.cs
@@ -167,6 +167,7 @@ namespace BankApp
 
             cmbLoanType.FlatStyle = FlatStyle.Flat;
             cmbLoanType.BackColor = Color.Transparent;
+            comboBox1.BackColor = this.BackColor;
             txtAmount.BorderStyle = BorderStyle.None;
             txtAmount.BackColor = Color.Transparent;
 
