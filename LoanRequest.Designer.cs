2c0d9f729dfc73ea58cae3fee11c2497390bc455
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-17 15:50:47 +0530

    Project Updated

--- LoanRequest.Designer.cs
+++ LoanRequest.Designer.cs
@@ -109,6 +109,7 @@ namespace BankApp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1280, 785);
             this.Controls.Add(this.dgvLoanRequests);
             this.Controls.Add(this.btnGoBack);
