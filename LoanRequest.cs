5d505005e1bc34e7318947cef4e8c7919fe320be
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-17 16:45:32 +0530

    Project Updated

--- LoanRequest.cs
+++ LoanRequest.cs
@@ -155,43 +155,12 @@ namespace BankApp
         
       
 
-        
-        private void ModernizeGrid(DataGridView grid)
-        {
-            grid.BorderStyle = BorderStyle.None;
-            grid.BackgroundColor = Color.White;
-            grid.EnableHeadersVisualStyles = false;
-            grid.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
-
-            // Header
-            grid.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
-            grid.ColumnHeadersDefaultCellStyle.BackColor = Color.White;
-            grid.ColumnHeadersDefaultCellStyle.ForeColor = Color.Black;
-            grid.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 10, FontStyle.Bold);
-            grid.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
-
-            // Rows
-            grid.DefaultCellStyle.BackColor = Color.White;
-            grid.DefaultCellStyle.ForeColor = Color.Black;
-            grid.DefaultCellStyle.Font = new Font("Segoe UI", 10);
-            grid.DefaultCellStyle.SelectionBackColor = Color.FromArgb(240, 248, 255);
-            grid.DefaultCellStyle.SelectionForeColor = Color.Black;
-
-            // Alternate row shading
-            grid.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(250, 250, 250);
-
-            // No row header
-            grid.RowHeadersVisible = false;
-            grid.RowTemplate.Height = 35;
-            foreach (DataGridViewRow row in dgvLoanRequests.Rows)
-            {
-              
-            }
-        }
+       
 
         private void LoanRequest_Load_1(object sender, EventArgs e)
         {
             dgvLoanRequests.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            GridHelper.ModernizeGrid(dgvLoanRequests);
 
         }
     }
