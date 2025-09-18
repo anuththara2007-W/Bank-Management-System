16f90fe0d68eb44405ec70a6a988c9ab6166c13d
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-18 20:50:46 +0530

    Project Updated

--- ReportsForm.Designer.cs
+++ ReportsForm.Designer.cs
@@ -28,89 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.cmbReportType = new System.Windows.Forms.ComboBox();
-            this.btnPreview = new System.Windows.Forms.Button();
-            this.btnExport = new System.Windows.Forms.Button();
-            this.dgvReport = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvReport)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(35, 23);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(165, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Generate Reports";
-            // 
-            // cmbReportType
-            // 
-            this.cmbReportType.FormattingEnabled = true;
-            this.cmbReportType.Items.AddRange(new object[] {
-            "Transactions",
-            "Loan Requests",
-            "Deposits",
-            "Withdrawals"});
-            this.cmbReportType.Location = new System.Drawing.Point(70, 71);
-            this.cmbReportType.Name = "cmbReportType";
-            this.cmbReportType.Size = new System.Drawing.Size(235, 24);
-            this.cmbReportType.TabIndex = 1;
-            // 
-            // btnPreview
-            // 
-            this.btnPreview.Location = new System.Drawing.Point(143, 127);
-            this.btnPreview.Name = "btnPreview";
-            this.btnPreview.Size = new System.Drawing.Size(151, 52);
-            this.btnPreview.TabIndex = 2;
-            this.btnPreview.Text = "Preview Report";
-            this.btnPreview.UseVisualStyleBackColor = true;
-            // 
-            // btnExport
-            // 
-            this.btnExport.Location = new System.Drawing.Point(405, 127);
-            this.btnExport.Name = "btnExport";
-            this.btnExport.Size = new System.Drawing.Size(151, 52);
-            this.btnExport.TabIndex = 3;
-            this.btnExport.Text = "Export to PDF";
-            this.btnExport.UseVisualStyleBackColor = true;
-            // 
-            // dgvReport
-            // 
-            this.dgvReport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvReport.Location = new System.Drawing.Point(20, 284);
-            this.dgvReport.Name = "dgvReport";
-            this.dgvReport.RowHeadersWidth = 51;
-            this.dgvReport.RowTemplate.Height = 24;
-            this.dgvReport.Size = new System.Drawing.Size(1022, 307);
-            this.dgvReport.TabIndex = 4;
             // 
             // ReportsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1093, 618);
-            this.Controls.Add(this.dgvReport);
-            this.Controls.Add(this.btnExport);
-            this.Controls.Add(this.btnPreview);
-            this.Controls.Add(this.cmbReportType);
-            this.Controls.Add(this.label1);
             this.Name = "ReportsForm";
             this.Text = "ReportsForm";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvReport)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox cmbReportType;
-        private System.Windows.Forms.Button btnPreview;
-        private System.Windows.Forms.Button btnExport;
-        private System.Windows.Forms.DataGridView dgvReport;
     }
 }
