03e3a0b1a9f6108a6996ca496617def8becada3f
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-19 16:06:46 +0530

    Project Updated

--- ChangePassword.Designer.cs
+++ ChangePassword.Designer.cs
@@ -104,7 +104,7 @@ namespace BankApp
             // 
             this.button1.Location = new System.Drawing.Point(707, 665);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(141, 47);
+            this.button1.Size = new System.Drawing.Size(186, 47);
             this.button1.TabIndex = 26;
             this.button1.Text = "Go Back";
             this.button1.UseVisualStyleBackColor = true;
