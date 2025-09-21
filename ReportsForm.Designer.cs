acab7f6777b58cf512e0422282598400c4b719e9
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-21 21:25:34 +0530

    Project Updated

--- ReportsForm.Designer.cs
+++ ReportsForm.Designer.cs
@@ -33,7 +33,9 @@
             // 
             // reportViewer1
             // 
-            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportViewer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.reportViewer1.Location = new System.Drawing.Point(0, 0);
             this.reportViewer1.Margin = new System.Windows.Forms.Padding(31, 3, 23, 3);
             this.reportViewer1.Name = "ReportViewer";
