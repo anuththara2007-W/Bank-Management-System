12da2269968aa97537aafbd5383997cc2764f456
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-21 22:11:55 +0530

    Project Updated

--- DepositWithdraw.cs
+++ DepositWithdraw.cs
@@ -213,6 +213,10 @@ namespace Bank__Management_System
         {
             gridAccounts.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             GridStyle.ModernizeGrid(gridAccounts);
+            lblAmount.BackColor = Color.Transparent;
+            lblAmount.BorderStyle = BorderStyle.None;
+            lblLoanType.BorderStyle = BorderStyle.None;
+            lblLoanType.BackColor = Color.Transparent;
         }
     }
 }
