11f9071e5c19102a9fe47bf7bac37fb5b278365a
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-09-21 21:52:37 +0530

    Project Updated

--- DepositWithdraw.Designer.cs
+++ DepositWithdraw.Designer.cs
@@ -102,6 +102,7 @@ namespace Bank__Management_System
             this.gridAccounts.AllowUserToAddRows = false;
             this.gridAccounts.AllowUserToDeleteRows = false;
             this.gridAccounts.AllowUserToResizeColumns = false;
+            this.gridAccounts.AllowUserToResizeRows = false;
             this.gridAccounts.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
             this.gridAccounts.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.gridAccounts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
