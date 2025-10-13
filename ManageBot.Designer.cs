6d2ba467a01f05be3a9261639d0026ca669ce472
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-10-13 11:40:32 +0530

    Project Updated

--- ManageBot.Designer.cs
+++ ManageBot.Designer.cs
@@ -67,7 +67,7 @@
             this.label1.BackColor = System.Drawing.Color.White;
             this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label1.Font = new System.Drawing.Font("Yu Gothic", 12F);
-            this.label1.Location = new System.Drawing.Point(942, 24);
+            this.label1.Location = new System.Drawing.Point(942, 30);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(127, 26);
             this.label1.TabIndex = 5;
