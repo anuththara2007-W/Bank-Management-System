8d9065559f44c75db37c21b7cf33cb20a7fca437
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-10-14 09:21:55 +0530

    Project Updated

--- ReportsForm.cs
+++ ReportsForm.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
+using Microsoft.Reporting.WinForms;
 
 namespace Bank__Management_System
 {
@@ -19,8 +15,33 @@ namespace Bank__Management_System
 
         private void ReportsForm_Load(object sender, EventArgs e)
         {
+            LoadTransactionsReport();
+        }
 
-            this.reportViewer1.RefreshReport();
+        private void LoadTransactionsReport()
+        {
+            // 🔹 1. Your SQL connection
+            string connectionString = "Data Source=YOUR_SERVER_NAME;Initial Catalog=YOUR_DATABASE_NAME;Integrated Security=True";
+            string query = "SELECT * FROM transactions";
+
+            // 🔹 2. Fetch data from SQL
+            DataTable dt = new DataTable();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                da.Fill(dt);
+            }
+
+            // 🔹 3. Set ReportViewer properties
+            reportViewer1.LocalReport.DataSources.Clear();
+            ReportDataSource rds = new ReportDataSource("BankDataSet_Transactions", dt);
+            reportViewer1.LocalReport.DataSources.Add(rds);
+
+            // 🔹 4. Link to RDLC file
+            reportViewer1.LocalReport.ReportPath = @"Reports\TransactionsReport.rdlc";
+
+            // 🔹 5. Refresh the report
+            reportViewer1.RefreshReport();
         }
     }
 }
