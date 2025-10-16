7bbe439b75800a30e0a220cb3ecc6872378078bc
Anuththara Wickramasekara <anuththarawickramasekara424@gmail.com>
2025-10-16 13:24:17 +0530

    Project Updated

--- Customer.cs
+++ Customer.cs
@@ -136,7 +136,7 @@ namespace Bank__Management_System
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Error updating: {ex.Message}");
+                MessageBox.Show("Error saving: " + ex.Message);
             }
         }
 
